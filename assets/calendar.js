4aa59eba5ce9714045d5e0686d015a0500e92870
MarynaPR <pryadkamaryna@gmail.com>
2020-10-02 11:23:27 -0500

    display current time

--- assets/calendar.js
+++ assets/calendar.js
@@ -1,8 +1,37 @@
-var dateFormat = ('MMMM Do YYYY, h:mm:ss a');
+//function to display the date
+var today = moment().format('MMMM Do YYYY, h:mm:ss a');
+var display = document.getElementById("currentDay");
+display.innerHTML = today;
+console.log("time");
 
-//daily planner to create a schedule-business hours
+
+
+
+
+//make interval that runs every sec. to refresh the time
+// setInterval(() => {
+//     //console.log("time update");
+//     var today = moment().format('MMMM Do YYYY, h:mm:ss a');
+
+// }, 1000);
+// $("#currentDay").text(currentTime);
+// console.log("moment", moment().hours());
+// var = events{ };
 //WHEN I open the planner -THEN the current day is displayed at the top of the calendar
-//WHEN I scroll down timeblocks for standard business hours 8-5?
-//each timeblock is color coded to indicate whether it is in the past, present, or future // when I click into a timeblock-THEN I can enter an event
-// WHEN I click the save button for that timeblock THEN the text for that event is saved in local storage
-//WHEN I refresh the page THEN the saved events persist
+
+//call function when open/refresh the page
+
+
+// WHEN I scroll down-THEN I am presented with timeblocks for standard business hours
+
+// WHEN I view the timeblocks for that day-THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+
+// WHEN I click into a timeblock-THEN I can enter an event
+
+// WHEN I click the save button for that timeblock-THEN the text for that event is saved in local storage
+// var saveEvents = function () {
+//     localStorage.setItem("events", JSON.stringify(events));
+// };
+// saveEvents();
+// WHEN I refresh the page-THEN the saved events persist
+
