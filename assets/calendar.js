4f09120f4a10103e1f95de15bdc28575a45ee36a
MarynaPR <pryadkamaryna@gmail.com>
2020-10-01 20:23:12 -0500

    adding links to HTML

---
+++ assets/calendar.js
@@ -0,0 +1,8 @@
+var dateFormat = ('MMMM Do YYYY, h:mm:ss a');
+
+//daily planner to create a schedule-business hours
+//WHEN I open the planner -THEN the current day is displayed at the top of the calendar
+//WHEN I scroll down timeblocks for standard business hours 8-5?
+//each timeblock is color coded to indicate whether it is in the past, present, or future // when I click into a timeblock-THEN I can enter an event
+// WHEN I click the save button for that timeblock THEN the text for that event is saved in local storage
+//WHEN I refresh the page THEN the saved events persist
