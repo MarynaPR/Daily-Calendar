de0cafbaf7b1a3eec822d6ca391dc351c8e75692
MarynaPR <pryadkamaryna@gmail.com>
2020-10-04 15:41:21 -0500

    removing comments from js file

--- assets/calendar.js
+++ assets/calendar.js
@@ -7,53 +7,44 @@ var display = document.getElementById("currentDay");
 display.innerHTML = today;
 console.log("time");
 
-//make interval that runs every min. to refresh the time
+//interval that runs every min to refresh the time
 setInterval(() => {
     console.log("time update");
     var today = moment().format('MMMM Do YYYY, h:mm:ss a');
-
 }, 10000);
 
 //what the user writes values(id)->put inside the js object->
 
-// WHEN I scroll down-THEN I am presented with timeblocks for standard business hours(8-5?)
-var toDo = [];
+// WHEN I scroll down-THEN I am presented with timeblocks for standard business hours 9-5
 // WHEN I click into a timeblock-THEN I can enter an event
-//in order to stop the page from reloading when the button is clicked, add default prevent
+var toDo = [];
 var addToDo = function (event, that) {
     console.log(that)
     console.log($(this));
-    event.preventDefault(); //stopts from reloading
+    //stops the page from reloading when the button is clicked
+    event.preventDefault();
     console.log(that.siblings("textarea").val())
     //create object
     var toDoEvent = {
-
-        id: Date.now(), hour: 0, text: "", //method to give current time stamp
-        //create properties of the object:
-        //event: document.getElementById("event").value, //value of what event was added by user, values for the properties inside the object
+        id: Date.now(), hour: 0, text: "",
+        //create properties of the object; value added by user=>values for the properties inside the object
     }
-    //();
-    toDo.push(toDoEvent);//ading event to the array of eventsC(created on top)
-    //document.querySelector("div-since inside the div?").reset(); to clear the text for the next entry
-    localStorage.setItem("listToDo", JSON.stringify(toDo)); //take the array of events we are going to create = toDo, not the object. Add array to the local storage
+    toDo.push(toDoEvent);//ading event to the array
+    localStorage.setItem("listToDo", JSON.stringify(toDo));
 }
-//add it to local storage
 $("button").on("click", function (event) {
     console.log($(this))
     var that = $(this);
     addToDo(event, that);
-
 })
 
 // WHEN I view the timeblocks for that day-THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-
 var updateHourBlocks = function () {
-    //use moments to get the current hour
+    //use momentsjs to get the current hour
     var currentHour = moment().hour();
-
     console.log(currentHour);
 
-    //loop over all the timeblocks,(use for loop instead)?
+    //loop over all the timeblocks
     $(".time-block").each(function () {
         var hourInput = parseInt($(this).attr("id").split("hour")[1]);
         //console.log(hourInput);
@@ -75,28 +66,19 @@ var updateHourBlocks = function () {
             $(this).removeAttr("class", "present row time-block");
             $(this).attr("class", "future row time-block");
         }
-
     });
-
 }
 updateHourBlocks();
 
 // WHEN I click the save button for that timeblock-THEN the text for that event is saved in local storage
 var saveToDo = function () {
-    //     localStorage.setItem("toDo", JSON.stringify(toDo));
-    //     console.log(saveToDo);
-    //     console.log(toDo);
-
     $(".saveBtn").on("click", function (event) {
         console.log("saveBtn");
-        //event.preventDefault();//?prevents from reloading
 
         var hour = $(this).parent().attr("id");
         var textToDo = $(this).siblings(".textarea").val();
 
         localStorage.setItem(hour, textToDo);
-        //console.log("textToDo");
-
         localStorage.setItem("hour9", $("#hour9 .description").val());
         localStorage.setItem("hour10", $("#hour10 .description").val());
         localStorage.setItem("hour11", $("#hour11 .description").val());
@@ -107,9 +89,7 @@ var saveToDo = function () {
         localStorage.setItem("hour16", $("#hour16 .description").val());
         localStorage.setItem("hour17", $("#hour17 .description").val());
     });
-
 };
-
 saveToDo();
 
 // WHEN I refresh the page-THEN the saved events persist
